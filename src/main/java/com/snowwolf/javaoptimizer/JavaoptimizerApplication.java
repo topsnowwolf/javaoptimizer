e69cfe98b57c57a5ba6cea520e3d502e2cb3e441
topsnowwolf <topsnowwolf@163.com>
2018-11-27 23:10:46 +0800

    Initial commit

---
+++ src/main/java/com/snowwolf/javaoptimizer/JavaoptimizerApplication.java
@@ -0,0 +1,12 @@
+package com.snowwolf.javaoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaoptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaoptimizerApplication.class, args);
+	}
+}
